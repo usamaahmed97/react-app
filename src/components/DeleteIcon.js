67a4e542bb3837c75f1d4345b10fcb2446448913
Usama Ahmed <usama.ahmed@emerald-labs.com>
2022-06-23 18:26:46 +0500

    Ruby Project Work

---
+++ src/components/DeleteIcon.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const DeleteIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18.924"
+      height="24.329"
+      viewBox="0 0 18.924 24.329"
+    >
+      <path
+        id="Icon_material-delete"
+        data-name="Icon material-delete"
+        d="M8.852,26.126a2.711,2.711,0,0,0,2.7,2.7H22.369a2.711,2.711,0,0,0,2.7-2.7V9.907H8.852ZM26.424,5.852H21.693L20.341,4.5H13.583L12.231,5.852H7.5v2.7H26.424Z"
+        transform="translate(-7.5 -4.5)"
+        fill="#d92e54"
+      />
+    </svg>
+  );
+};
+
+export default DeleteIcon;
